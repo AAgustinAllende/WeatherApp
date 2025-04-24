13d55e049391c59b60787953bb4738c08b9a7200
Agustin Allende <agus.allende996@gmail.com>
2025-04-24 11:28:34 -0300

    moviendo la api key a las variables de entorno

--- src/components/WeatherPanel.js
+++ src/components/WeatherPanel.js
@@ -1,73 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Form from './Form';
 import Card from './Card';
 
 const WeatherPanel = () => {
 
-    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=85139334587e2a8f67d242022b9b31a9&lang=es";
+    let urlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&lang=es`;
     let cityUrl = "&q=";
 
-    let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=85139334587e2a8f67d242022b9b31a9&lang=es"
+    let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&lang=es`;
 
     const [weather, setWeather] = useState([]);
-    const [forecast, setForecast] = useState([])
-    const [loading,setLoading] = useState(false)
-    const [show, setShow] = useState(false)
-    const [location, setLocation] = useState("")
+    const [forecast, setForecast] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
 
-    const getLocation = async (loc) =>{
+    const getLocation = async (loc) => {
         setLoading(true);
         setLocation(loc);
 
         // weather
-    urlWeather = urlWeather + cityUrl + loc;
+        let weatherUrl = urlWeather + cityUrl + loc;
+        await fetch(weatherUrl)
+            .then((response) => {
+                if (!response.ok) throw { response };
+                return response.json();
+            })
+            .then((weatherData) => {
+                console.log(weatherData);
+                setWeather(weatherData);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+                setShow(false);
+            });
 
-    await fetch(urlWeather).then ((response) => {
-        if(!response.ok) throw {response}
-        return response.json();
-    }).then((weatherData) => {
-        console.log(weatherData);
-        setWeather(weatherData);
-    }).catch(error => {
-        console.log(error);
-        setLoading(false);
-        setShow(false);
-    });
+        // Forecast
+        let forecastUrl = urlForecast + cityUrl + loc;
+        await fetch(forecastUrl)
+            .then((response) => {
+                if (!response.ok) throw { response };
+                return response.json();
+            })
+            .then((forecastData) => {
+                console.log(forecastData);
+                setForecast(forecastData);
+                setLoading(false);
+                setShow(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+                setShow(false);
+            });
+    };
 
-    //Forecast
-
-    urlForecast = urlForecast + cityUrl + loc;
-
-    await fetch(urlForecast).then ((response) => {
-        if(!response.ok) throw {response}
-        return response.json();
-    }).then((forecastData) => {
-        console.log(forecastData);
-        setForecast(forecastData);
-        setLoading(false);
-        setShow(true);
-    }).catch(error => {
-        console.log(error);
-        setLoading(false);
-        setShow(false);
-    });
-    }
-
-    return(
+    return (
         <React.Fragment>
-            <Form 
-            newLocation = {getLocation}
-            />
+            <Form newLocation={getLocation} />
             <Card
-            showData={show}
-            loadingData = {loading}
-            weather = {weather}
-            forecast = {forecast}
+                showData={show}
+                loadingData={loading}
+                weather={weather}
+                forecast={forecast}
             />
         </React.Fragment>
     );
-
-    
 }
 
-export default WeatherPanel
+export default WeatherPanel;
